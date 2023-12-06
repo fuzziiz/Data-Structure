29d133f528cf98c5c5a17dea9b49af49632ce5f1
CLINT UBANAN <148334301+fuzziiz@users.noreply.github.com>
2023-12-06 12:24:54 +0800

    Update PostfixCalculator.java

--- PostfixCalculator/PostfixCalculator.java
+++ PostfixCalculator/PostfixCalculator.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
-class PostfixCalculator {
-
-    private static int precedence(char ch) {
+public class PostfixCalculator {
+    static int Prec(char ch) {
         switch (ch) {
             case '+':
             case '-':
@@ -16,90 +14,102 @@ class PostfixCalculator {
         }
         return -1;
     }
+    
+    static String infixToPostfix(String exp) {
+        String result = "";
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < exp.length(); ++i) {
+            char c = exp.charAt(i);
 
-    private static int applyOperation(int a, int b, char operator) {
-        switch (operator) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            case '/':
-                if (b == 0) throw new UnsupportedOperationException("Cannot divide by zero");
-                return a / b;
-        }
-        return 0;
-    }
+            if (Character.isWhitespace(c)) {
+                continue;
+            }
 
-    private static int evaluatePostfix(String expression, boolean showSteps) {
-        Stack<Integer> stack = new Stack<>();
-        StringBuilder steps = new StringBuilder();
-        for (char c : expression.toCharArray()) {
-            if (Character.isDigit(c)) {
-                stack.push(c - '0');
-            } else {
-                int val1 = stack.pop();
-                int val2 = stack.pop();
-                int result = applyOperation(val2, val1, c);
-                stack.push(result);
-                if (showSteps) {
-                    steps.append("-> ").append(val2).append(val1).append(c).append("\n");
-                    steps.append("-> ").append(stack.peek()).append(val2).append(val1).append(c).append("\n");
+            if (Character.isLetterOrDigit(c)) {
+                result += c;
+            } 
+            else if (c == '(') {
+                stack.push(c);
+            } 
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result += stack.pop();
                 }
+                stack.pop();
+            } 
+            else {
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek()) && stack.peek() != '(') {
+                    result += stack.pop();
+                }
+                stack.push(c);
             }
         }
-        if (showSteps) {
-            System.out.println("Postfix Solution: ");
-            System.out.println(steps);
+
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+
+    
+    static double evaluatePostfix(String expression) {
+        Deque<Double> stack = new ArrayDeque<Double>();
+
+        for (int i = 0; i < expression.length(); ++i) {
+            char c = expression.charAt(i);
+
+            if (Character.isDigit(c)) {
+                stack.push((double) (c - '0'));
+            } else {
+                try {
+                    double op2 = stack.pop();
+                    double op1 = stack.pop();
+                    double result = 0;
+
+                    switch (c) {
+                        case '+':
+                            result = op1 + op2;
+                            break;
+                        case '-':
+                            result = op1 - op2;
+                            break;
+                        case '*':
+                            result = op1 * op2;
+                            break;
+                        case '/':
+                            if (op2 != 0) {
+                                result = op1 / op2;
+                            } else {
+                                throw new ArithmeticException("Invalid Division by Zero");
+                            }
+                            break;
+                        case '^':
+                            result = Math.pow(op1, op2);
+                            break;
+                    }
+                    stack.push(result);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e.getMessage());
+                    System.exit(1);
+                }
+            }
         }
         return stack.pop();
     }
-
-    private static String infixToPostfix(String expression) {
-        StringBuilder result = new StringBuilder();
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : expression.toCharArray()) {
-            if (Character.isDigit(c)) {
-                result.append(c);
-            } else if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    result.append(stack.pop());
-                }
-                if (!stack.isEmpty() && stack.peek() != '(') {
-                    throw new IllegalArgumentException("Invalid Expression");
-                } else {
-                    stack.pop();
-                }
-            } else {
-                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
-                    result.append(stack.pop());
-                }
-                stack.push(c);
-            }
-        }
-        while (!stack.isEmpty()) {
-            result.append(stack.pop());
-        }
-        return result.toString();
-    }
-
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("Enter expression: ");
-        String expression = scan.nextLine();
-
         try {
-            String postfix = infixToPostfix(expression);
-            System.out.println("Postfix Expression: " + postfix);
-            int result = evaluatePostfix(postfix, true);
-            System.out.println("Result: " + result);
-        } catch (UnsupportedOperationException | IllegalArgumentException e) {
+            System.out.print("Enter Expression: ");
+            String expression = scan.nextLine();
+            System.out.println("Postfix Expression: " + infixToPostfix(expression));
+            System.out.println("Result: " + evaluatePostfix(infixToPostfix(expression)));
+        } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
+        } finally {
+            scan.close();
         }
     }
 }
